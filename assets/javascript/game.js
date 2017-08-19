6f5056b113fa39950e12ea89ff3c4183642cce3d
ruthieirl <ruthfromjersey@gmail.com>
2017-08-18 21:24:12 -0400

    more progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,35 +5,45 @@ $(document).ready(function() {
 	var wins = 0;
 	var losses = 0; 
 	var score = "";
+
 	//Computer needs to generate random number between 19 and 120. 
 	//PROBLEM: Random number goes up to 120 but I saw 16 pop up, so something is wrong.
 	var targetNumber = Math.floor(Math.random() * 120) + 1;
+
 	//Takes the target number and places it in the correct div for display.
 	$("#targetNumber").html("<h3>" + targetNumber + "</h3>");
+
 	//Computer needs to generate the four images and random values of crystal buttons.
 	//Values are between 1 and 12.
-	var valueOptions = [1, 5, 9, 11];
+	var valueOptions = [1, 5, 11, 9];
+
 	//Create an array of image sources for crystals
 	var images = ["assets/images/greenish.png", "assets/images/bluish.png", "assets/images/purplish.png", "assets/images/pinkish.png"]
+
 	//Create a loop to create crystals for each value in the array
 	for (var i = 0; i < valueOptions.length; i++) {
 		//Each time it goes through, it should choose an image.
 		var crystal = $("<img>");
 		console.log(valueOptions[i]);
 		crystal.addClass("images");
-		//This only assigns this particular pic.  How to use pics from array???
-		crystal.attr("src", "assets/images/greenish.png");
+		crystal.addClass("btn");
 		crystal.attr("data-crystalvalue", valueOptions[i]);
 		$(".imageHolder").append(crystal);
-
 	}
+
+	//Set each of the four images
+	$.each(images, function (index, value){
+		$(".imageHolder").append("<img src=" + value + " width='150px'>");
+	});
+
 	//Every time a crystal button is clicked, the value of the button is added. 
+	//PROBLEM: Now it doesn't seem to be "clicked"
 	$("body").on("click", ".btn", function() {
 		var crystalValue = ($(this).attr("data-crystalvalue"));
     	crystalValue = parseInt(crystalValue);
-    	//Recognizes my clicks, but the values are undefined.
     	console.log($(this).attr("data-crystalvalue"))
 	});
+
 	//If the user number and computer's random number match, user wins. Game resets.
 	if (targetNumber === score) {
 		wins++;
