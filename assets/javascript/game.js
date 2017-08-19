75ddd89ab1c380156cbd51c4e714c86de8e1aaa7
ruthieirl <ruthfromjersey@gmail.com>
2017-08-18 23:03:45 -0400

    more progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	//Set up variables to hold our numbers.
 	var wins = 0;
 	var losses = 0; 
-	var score = "";
+	var counter = 0;
 
 	//Computer needs to generate random number between 19 and 120. 
 	//PROBLEM: Random number goes up to 120 but I saw 16 pop up, so something is wrong.
@@ -16,51 +16,51 @@ $(document).ready(function() {
 	//Computer needs to generate the four images and random values of crystal buttons.
 	//Values are between 1 and 12.
 	var valueOptions = [1, 5, 11, 9];
+	console.log(valueOptions);
 
 	//Create an array of image sources for crystals
 	var images = ["assets/images/greenish.png", "assets/images/bluish.png", "assets/images/purplish.png", "assets/images/pinkish.png"]
 
-	//Create a loop to create crystals for each value in the array
-	for (var i = 0; i < valueOptions.length; i++) {
-		//Each time it goes through, it should choose an image.
-		var crystal = $("<img>");
-		console.log(valueOptions[i]);
-		crystal.addClass("images");
-		crystal.addClass("btn");
-		crystal.attr("data-crystalvalue", valueOptions[i]);
-		$(".imageHolder").append(crystal);
-	}
-
 	//Set each of the four images
-	$.each(images, function (index, value){
-		$(".imageHolder").append("<img src=" + value + " width='150px'>");
+	$.each(images, function (index, value) {
+		$("#imageHolder").append("<img src=" + value + " width='150px'>");
+		console.log(index + " : " + value);
 	});
+
+	//Create loop to assign value
+	for (var i = 0; i < valueOptions.length; i++) {
+		$("img").addClass("btn")
+		$(".btn").attr("data-crystalvalue", valueOptions[i]);
+		console.log("data-crystalvalue");
+	}
 
 	//Every time a crystal button is clicked, the value of the button is added. 
 	//PROBLEM: Now it doesn't seem to be "clicked"
 	$("body").on("click", ".btn", function() {
+		console.log(this);
 		var crystalValue = ($(this).attr("data-crystalvalue"));
     	crystalValue = parseInt(crystalValue);
-    	console.log($(this).attr("data-crystalvalue"))
+    	console.log(crystalValue);
+    	console.log($(this).attr("data-crystalvalue"));
 	});
 
 	//If the user number and computer's random number match, user wins. Game resets.
-	if (targetNumber === score) {
+	if (targetNumber === counter) {
 		wins++;
 		$("#scoreboard").html("<h2>Wins: " + wins + "</h2>" +
 							  "<h2>Losses: " + losses + "</h2>");
 		console.log(wins);
 		console.log(losses);
-		score = 0;
+		counter = 0;
 
 	}
 	//If user number is greater than computer's random number, user loses. Game resets.
-	if (score > targetNumber) {
+	if (counter > targetNumber) {
 		losses++;
 		$("#scoreboard").html("<h2>Wins: " + wins + "</h2>" +
 							  "<h2>Losses: " + losses + "</h2>");
 		console.log(wins);
 		console.log(losses);
-		score = 0;
+		counter = 0;
 	}
 }); //$(document).ready();
